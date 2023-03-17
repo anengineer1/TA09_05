c4f7a14b71550fb9aeaa65ef19e81eb709d2846e
Francisco Parra <franparpe@disroot.org>
2023-03-17 12:04:13 +0100

    Primer esbozo superclase

---
+++ src/superclasses/PersonaEnClase.java
@@ -0,0 +1,57 @@
+package superclasses;
+
+public class PersonaEnClase {
+	protected String nombre;
+	protected String edad;
+	protected String sexo;
+	protected double probabilidad_asistencia_porcentaje;
+	protected boolean asiste;
+
+	public PersonaEnClase(String nombre, String edad, String sexo, double probabilidad_asistencia_porcentaje) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.probabilidad_asistencia_porcentaje = probabilidad_asistencia_porcentaje;
+	}
+
+	protected void calculaAsistencia() {
+		double valor_aleatorio = Math.random() * 100;
+		this.asiste = (probabilidad_asistencia_porcentaje > valor_aleatorio);
+	}
+
+	public boolean isDisponible() {
+		return asiste;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getEdad() {
+		return edad;
+	}
+
+	public double getProbabilidad_asistencia_porcentaje() {
+		return probabilidad_asistencia_porcentaje;
+	}
+
+	public boolean isAsiste() {
+		return asiste;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+
+	public void setProbabilidad_asistencia_porcentaje(double probabilidad_asistencia_porcentaje) {
+		this.probabilidad_asistencia_porcentaje = probabilidad_asistencia_porcentaje;
+	}
+
+	public void setAsiste(boolean asiste) {
+		this.asiste = asiste;
+	}
+}
